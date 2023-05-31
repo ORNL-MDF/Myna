25c5bac2e1b4d057ee5bdb554957fac8cd32d9e4
Knapp, Gerry <knappgl@ornl.gov>
2023-05-31 13:53:43 +0000

    convert autofoam_case_gen.py rel paths to abs
    Former-commit-id: 2a54db25466e062acbc62651016c9db9d7ea1c3c

--- autofoam_case_gen.py
+++ autofoam_case_gen.py
@@ -2,6 +2,19 @@ import autofoam
 import autofoam.mesh
 import autofoam.cases
 import json
+import os
+
+def nested_set(dict, keys, value):
+    ''' modifies a nested dictionary value given a list of keys to the nested location'''
+    for key in keys[:-1]:
+        dict = dict.setdefault(key, {})
+    dict[keys[-1]] = value
+
+def nested_get(dict, keys):
+    ''' modifies a nested dictionary value given a list of keys to the nested location'''
+    for key in keys[:-1]:
+        dict = dict.setdefault(key, {})
+    return dict[keys[-1]]
 
 def generate(inputs):
     # Preprocess STL
@@ -20,7 +33,32 @@ def generate(inputs):
 
 
 if __name__ == "__main__":
+
     with open("autofoam_inputs.json", "r") as f:
         inputs = json.load(f)
+
+    path_inputs = [
+        ["mesh","stl_path"],
+        ["template","template_dir"],
+        ["case_dir"]]
+    
+    for path_input in path_inputs:
+        path = nested_get(inputs, path_input)
+        if not os.path.isabs(path):
+            new_path = os.path.abspath(path)
+            nested_set(inputs, path_input, new_path)
+            print(path, new_path)
+    
+    for case in inputs["cases"].keys():
+        print(case)
+        case_path = inputs["cases"][case]["scan_path"]
+        path_input = ["cases", case, "scan_path"]
+        if not os.path.isabs(case_path):
+            new_path = os.path.abspath(case_path)
+            nested_set(inputs, path_input, new_path)
+            print(case_path, new_path)
+
+    with open("autofoam_inputs_abs.json", "w") as f:
+        json.dump(inputs, f, indent=3)
 
     generate(inputs)
