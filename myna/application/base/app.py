24b64bdb3c125f9ff1afd51f1d836692210bb1a2
Sam Reeve <6740307+streeve@users.noreply.github.com>
2024-07-25 11:56:57 -0400

    Add new options to base app from thesis
    Former-commit-id: 2f3eef1d23273ca03d405a44a345217f85fbf2ea

--- myna/application/base/app.py
+++ myna/application/base/app.py
@@ -31,8 +31,37 @@ class MynaApp:
             + " then assume default location",
         )
         self.parser.add_argument(
+            "--overwrite",
+            dest="overwrite",
+            action="store_true",
+            help="force regeneration of each run and overwrite of any existing data",
+        )
+        self.parser.add_argument(
             "--exec", type=str, help=f"(str) Path to {self.name} executable"
         )
+        self.parser.add_argument(
+            "--np",
+            default=8,
+            type=int,
+            help="(int) processors to use per job, will "
+            + "correct to the maximum available processors if "
+            + "set too large",
+        )
+        self.parser.add_argument(
+            "--maxproc",
+            default=None,
+            type=int,
+            help="(int) maximum available processors for system, will "
+            + "correct to the maximum available processors if "
+            + "set too large",
+        )
+        self.parser.add_argument(
+            "--batch",
+            dest="batch",
+            action="store_true",
+            help="(flag) run jobs in parallel",
+        )
+        self.parser.set_defaults(batch=False)
 
     # Check if executable exists
     def check_exe(self, *path_args):
@@ -48,6 +77,15 @@ class MynaApp:
             raise Exception(
                 f'The specified {self.name} executable "{exe}" is not executable.'
             )
+
+    # args must have been parsed
+    def set_procs(self):
+        # Set processor information
+        self.np = self.args.np
+        self.maxproc = self.args.maxproc
+        if self.maxproc is None:
+            self.maxproc = os.cpu_count()
+        self.np = min(os.cpu_count(), self.np, self.maxproc)
 
     def set_template_path(self, *path_args):
         if self.template is None:
--- myna/application/exaca/microstructure_region/configure.py
+++ myna/application/exaca/microstructure_region/configure.py
@@ -19,7 +19,8 @@ def setup_case(
 ):
 
     # Copy template to case directory
-    shutil.copytree(sim.args.template, case_dir, dirs_exist_ok=True)
+    if (not os.path.exists(sim.args.template)) or (sim.args.overwrite):
+        shutil.copytree(sim.args.template, case_dir, dirs_exist_ok=True)
 
     # Get case settings and template input JSON
     myna_settings = load_input(os.path.join(case_dir, "myna_data.yaml"))
