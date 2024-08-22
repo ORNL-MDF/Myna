c87a6eff41a7fd09dfa585836b19f5fe7b1cec93
Sam Reeve <6740307+streeve@users.noreply.github.com>
2024-08-22 15:16:55 -0400

    Get version automatically (this test is just to make sure something is passing)

--- tests/test_version.py
+++ tests/test_version.py
@@ -8,10 +8,16 @@
 #
 import pytest
 from importlib.metadata import version
+import os
 
 import myna
 
 
 # This test is primarily intended to ensure the test suite itself is working.
 def test_version():
-    assert version("myna") == "1.1.0.dev0"
+    path = os.path.dirname(os.path.abspath(__file__))
+    with open( os.path.join(path, "..", "pyproject.toml") )as f:
+        lines = f.readlines()
+
+    toml_version = lines[["version = " in l for l in lines].index(True)].split(" = ")[-1].split('"')[1]
+    assert (version("myna") == toml_version)
