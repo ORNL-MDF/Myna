ff9a2dc0a01438133d9b5f36ab758c4c8cea650a
Sam Reeve <6740307+streeve@users.noreply.github.com>
2024-08-22 12:11:25 -0400

    fixup: test version

--- tests/test_version.py
+++ tests/test_version.py
@@ -14,4 +14,4 @@ import myna
 
 # This test is primarily intended to ensure the test suite itself is working.
 def test_version():
-    assert version("myna") == "0.1.0.dev0"
+    assert version("myna") == "1.1.0.dev0"
