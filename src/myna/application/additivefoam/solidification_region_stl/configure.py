35c8a1a0d7680eb5504dcc0ff525324310a67689
Gerry Knapp <knappgl@ornl.gov>
2024-12-10 11:56:17 -0500

    refactor additivefoam apps to move shared functions to AdditiveFOAM(MynaApp) class

--- src/myna/application/additivefoam/solidification_region_stl/configure.py
+++ src/myna/application/additivefoam/solidification_region_stl/configure.py
@@ -7,24 +7,20 @@
 # License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause.
 #
 import os
-import argparse
-import sys
-import subprocess
 import shutil
-import pandas as pd
-import numpy as np
 import yaml
 
 from myna.core.workflow.load_input import load_input
-import myna.application.openfoam as openfoam
+from myna.application import openfoam
 from myna.application.additivefoam import AdditiveFOAM
 from myna.application.additivefoam.path import convert_peregrine_scanpath
 
 
 def setup_case(case_dir, app):
-    settings = app.settings
-    input_dir = os.path.dirname(settings["myna"]["input"])
-    resource_dir = os.path.join(input_dir, "myna_resources")
+    """Create a valid AdditiveFOAM case directory based on the myna_data.yaml file"""
+
+    # Get case settings
+    settings = load_input(os.path.join(case_dir, "myna_data.yaml"))
 
     # Generate case information from RVE list
     build = settings["build"]["name"]
@@ -36,231 +32,116 @@ def setup_case(case_dir, app):
     layer_dict = region_dict["layer_data"][layer]
 
     # Set directory for template mesh
-    resource_template_dir = os.path.join(
-        resource_dir,
-        part,
-        region,
-        "additivefoam",
-        "solidification_region_stl",
-        "template",
-    )
+    template_dir = app.get_region_resource_template_dir(part, region)
+    template_dir_abs = os.path.abspath(template_dir)
 
-    # Get scan path and layer thickness
+    # Get case metadata
     myna_scanfile = layer_dict["scanpath"]["file_local"]
     layer_thickness = settings["build"]["build_data"]["layer_thickness"]["value"]
-
-    # Get STL file location
     stl_path = part_dict["stl"]["file_local"]
 
-    # Set and write template background mesh dictionary
-    # for checking if background mesh needs to be regenerated
-    # TODO: Make sure this contains all needed fields for checking
-    # if the mesh needs to be updated
+    # If a template STL mesh dict exists, then test match to current STL mesh dict
     template_stl_mesh_dict = {
         "build": build,
         "part": part,
         "region": region,
         "coarse_mesh": app.args.coarse,
-    }
-    template_region_mesh_dict = {
-        "rx": app.args.rx,
-        "ry": app.args.ry,
-        "rz": app.args.rz,
-        "region_pad": app.args.region_pad,
-        "depth_pad": app.args.depth_pad,
-        "substrate_pad": app.args.substrate_pad,
-        "refine_layer": app.args.refine_layer,
-        "refine_region": app.args.refine_region,
+        "stl": stl_path,
     }
     template_stl_mesh_dict_name = "template_stl_mesh_dict.yaml"
     template_stl_mesh_dict_path = os.path.join(
-        resource_template_dir, template_stl_mesh_dict_name
+        template_dir_abs, template_stl_mesh_dict_name
     )
-    template_region_mesh_dict_name = "template_region_mesh_dict.yaml"
-    template_region_mesh_dict_path = os.path.join(
-        resource_template_dir, template_region_mesh_dict_name
-    )
-
-    # Write template STL mesh dict as needed, and if the template
-    # STL mesh dict exists, then check if it matches current mesh settings
     use_existing_stl_mesh = app.has_matching_template_mesh_dict(
         template_stl_mesh_dict_path, template_stl_mesh_dict
     )
     if not use_existing_stl_mesh:
-        app.copy_template_to_dir(resource_template_dir)
+        app.copy_template_to_dir(template_dir_abs)
         with open(template_stl_mesh_dict_path, "w", encoding="utf-8") as f:
             yaml.dump(template_stl_mesh_dict, f, default_flow_style=None)
 
-    # Write template region mesh dict as needed, and if the template region
-    # mesh dict exists, then check if it matches current mesh settings
+    # If a template region mesh dict exists, then test match to current region mesh dict
+    template_region_mesh_dict = {
+        "build": build,
+        "part": part,
+        "region": region,
+        "rx": app.args.rx,
+        "ry": app.args.ry,
+        "rz": app.args.rz,
+        "region_pad": app.args.pad_xy,
+        "depth_pad": app.args.pad_z,
+        "substrate_pad": app.args.pad_sub,
+        "coarse_mesh": app.args.coarse,
+        "refine_layer": app.args.refine_layer,
+        "refine_region": app.args.refine_region,
+    }
+    template_region_mesh_dict_name = "template_region_mesh_dict.yaml"
+    template_region_mesh_dict_path = os.path.join(
+        template_dir_abs, template_region_mesh_dict_name
+    )
     use_existing_region_mesh = app.has_matching_template_mesh_dict(
         template_region_mesh_dict_path, template_region_mesh_dict
     )
     if not use_existing_region_mesh:
         with open(template_region_mesh_dict_path, "w", encoding="utf-8") as f:
-            yaml.dump(template_region_mesh_dict_name, f, default_flow_style=None)
+            yaml.dump(template_region_mesh_dict, f, default_flow_style=None)
 
-    # Set input dictionary in format required by functions
-    additivefoam_input_dict = {
-        "scan_path": myna_scanfile,
-        "layer": layer,
-        "layer_thickness": layer_thickness,
-        "layer_box": [
-            [
-                float(region_dict["x"] - 0.5 * app.args.rx - app.args.region_pad),
-                float(region_dict["y"] - 0.5 * app.args.ry - app.args.region_pad),
-                float(-app.args.rz - app.args.depth_pad),
-            ],
-            [
-                float(region_dict["x"] + 0.5 * app.args.rx + app.args.region_pad),
-                float(region_dict["y"] + 0.5 * app.args.ry + app.args.region_pad),
-                float(0.0),
-            ],
+    # Set bounding boxes for mesh
+    layer_box = [
+        [
+            float(region_dict["x"] - 0.5 * app.args.rx - app.args.pad_xy),
+            float(region_dict["y"] - 0.5 * app.args.ry - app.args.pad_xy),
+            float(-app.args.rz - app.args.pad_z),
         ],
-        "region_box": [
-            [
-                float(region_dict["x"] - 0.5 * app.args.rx),
-                float(region_dict["y"] - 0.5 * app.args.ry),
-                float(-app.args.rz),
-            ],
-            [
-                float(region_dict["x"] + 0.5 * app.args.rx),
-                float(region_dict["y"] + 0.5 * app.args.ry),
-                float(0.0),
-            ],
+        [
+            float(region_dict["x"] + 0.5 * app.args.rx + app.args.pad_xy),
+            float(region_dict["y"] + 0.5 * app.args.ry + app.args.pad_xy),
+            float(0.0),
         ],
-        "rve_pad": [
-            app.args.region_pad,
-            app.args.region_pad,
-            app.args.depth_pad + app.args.substrate_pad,
+    ]
+    region_box = [
+        [
+            float(region_dict["x"] - 0.5 * app.args.rx),
+            float(region_dict["y"] - 0.5 * app.args.ry),
+            float(-app.args.rz),
         ],
-        "case_dir": case_dir,
-        "template": {"template_dir": resource_template_dir},
-        "mesh": {
-            "spacing": [app.args.coarse, app.args.coarse, app.args.coarse],
-            "tolerance": 1.0e-08,
-            "refinement": 0,
-            "refine_layer": app.args.refine_layer,
-            "refine_region": app.args.refine_region + app.args.refine_layer,
-            "stl_path": stl_path,
-            "convertToMeters": 1.0e-3,
-            "layer_thickness": layer_thickness,
-            "scaling": app.args.scale_factor,
-        },
-        "exe": {"nProcs": app.args.cores, "mpiArgs": f"mpirun -np {app.args.cores}"},
-    }
-
-    # Generate cases based on inputs
-    generate(
-        additivefoam_input_dict,
-        settings,
-        use_existing_stl_mesh,
-        use_existing_region_mesh,
-        app,
-    )
-
-    return
-
-
-def generate(
-    additivefoam_input_dict,
-    myna_settings,
-    use_existing_stl_mesh,
-    use_existing_region_mesh,
-    app,
-):
-    # Set paths
-    case_dir = additivefoam_input_dict["case_dir"]
-    template_dir = os.path.abspath(additivefoam_input_dict["template"]["template_dir"])
-
-    # Extract the laser power and spot size from the myna settings
-    part = list(myna_settings["build"]["parts"].keys())[0]
-    part_dict = myna_settings["build"]["parts"][part]
-    power = part_dict["laser_power"]["value"]  # W
-    spot_size = (
-        0.5 * part_dict["spot_size"]["value"] * 1e-3
-    )  # diameter -> radius & mm -> m
+        [
+            float(region_dict["x"] + 0.5 * app.args.rx),
+            float(region_dict["y"] + 0.5 * app.args.ry),
+            float(0.0),
+        ],
+    ]
+    rve_pad = [
+        app.args.pad_xy,
+        app.args.pad_xy,
+        app.args.pad_z + app.args.pad_sub,
+    ]
 
     # Convert the Myna scan path file
-    path_name = os.path.basename(additivefoam_input_dict["scan_path"])
+    path_name = os.path.basename(myna_scanfile)
     new_scan_path_file = os.path.join(template_dir, "constant", path_name)
-
-    convert_peregrine_scanpath(
-        additivefoam_input_dict["scan_path"], new_scan_path_file, power
-    )
-
-    #####################
-    # Set the beam size #
-    #####################
-    # 1. Get heatSourceModel
-    heat_source_model = (
-        subprocess.check_output(
-            f"foamDictionary -entry beam/heatSourceModel -value "
-            + f"{template_dir}/constant/heatSourceDict",
-            shell=True,
-        )
-        .decode("utf-8")
-        .strip()
-    )
-
-    # 2. Get heatSourceModelCoeffs/dimensions
-    heat_source_dimensions = (
-        subprocess.check_output(
-            f"foamDictionary -entry beam/{heat_source_model}Coeffs/dimensions -value "
-            + f"{template_dir}/constant/heatSourceDict",
-            shell=True,
-        )
-        .decode("utf-8")
-        .strip()
-    )
-    heat_source_dimensions = (
-        heat_source_dimensions.replace("(", "").replace(")", "").strip()
-    )
-    heat_source_dimensions = [float(x) for x in heat_source_dimensions.split(" ")]
-
-    # 3. Modify X- and Y-dimensions
-    heat_source_dimensions[:2] = [spot_size, spot_size]
-    heat_source_dimensions = [round(dim, 7) for dim in heat_source_dimensions]
-
-    # 4. Write to file
-    heat_source_dim_string = (
-        str(heat_source_dimensions)
-        .replace("[", "( ")
-        .replace("]", " )")
-        .replace(",", "")
-    )
-    os.system(
-        f'foamDictionary -entry beam/{heat_source_model}Coeffs/dimensions -set "{heat_source_dim_string}" '
-        + f"{template_dir}/constant/heatSourceDict"
-    )
-
-    ###################
-    # Mesh generation #
-    ###################
-    rve = additivefoam_input_dict["region_box"]
-    rve_pad = additivefoam_input_dict["rve_pad"]  # convert from float to XYZ list
+    power = part_dict["laser_power"]["value"]  # W
+    convert_peregrine_scanpath(myna_scanfile, new_scan_path_file, power)
 
     # If needed, generate AdditiveFOAM mesh in template folder
     if not use_existing_stl_mesh:
 
         # Preprocess the STL file
-        stl_path = additivefoam_input_dict["mesh"]["stl_path"]
-        scale_factor = additivefoam_input_dict["mesh"]["scaling"]
         working_stl_path = openfoam.mesh.preprocess_stl(
-            template_dir, stl_path, scale_factor
+            template_dir, stl_path, app.args.scale
         )
 
         # Generate background mesh
-        origin, bbDict = openfoam.mesh.create_background_mesh(
+        origin, bb_dict = openfoam.mesh.create_background_mesh(
             template_dir,
             working_stl_path,
-            additivefoam_input_dict["mesh"]["spacing"],
-            additivefoam_input_dict["mesh"]["tolerance"],
+            [app.args.coarse, app.args.coarse, app.args.coarse],
+            1.0e-08,
         )
         openfoam.mesh.extract_stl_features(
             template_dir,
             working_stl_path,
-            additivefoam_input_dict["mesh"]["refinement"],
+            0,
             origin,
         )
 
@@ -268,136 +149,67 @@ def generate(
         openfoam.mesh.create_part_mesh(
             template_dir,
             working_stl_path,
-            bbDict,
-            additivefoam_input_dict["exe"]["mpiArgs"],
+            bb_dict,
+            f"mpirun -np {app.args.np}",
         )
 
     else:
         # get the bounding box information based on specified RVE
         bb_min = [
-            rve[0][0] - rve_pad[0],
-            rve[0][1] - rve_pad[1],
-            rve[0][2] - rve_pad[2],
+            region_box[0][0] - rve_pad[0],
+            region_box[0][1] - rve_pad[1],
+            region_box[0][2] - rve_pad[2],
         ]
-        bb_max = [rve[1][0] + rve_pad[0], rve[1][1] + rve_pad[1], rve[1][2]]
+        bb_max = [
+            region_box[1][0] + rve_pad[0],
+            region_box[1][1] + rve_pad[1],
+            region_box[1][2],
+        ]
         bb = bb_min + bb_max
-        bbDict = {"bb_min": bb_min, "bb_max": bb_max, "bb": bb}
+        bb_dict = {"bb_min": bb_min, "bb_max": bb_max, "bb": bb}
 
     if not use_existing_region_mesh:
         # Slice the mesh for the given layer
-        height = float(additivefoam_input_dict["layer_thickness"]) * float(
-            additivefoam_input_dict["layer"]
-        )
+        height = float(layer_thickness) * float(layer)
         print("height = ", height)
         openfoam.mesh.slice(template_dir, height)
 
         # Generate refined mesh in layer thickness
-        refinement = additivefoam_input_dict["mesh"]["refine_layer"]
+        refinement = app.args.refine_layer
         refine_dict_path = os.path.join(template_dir, "system", "refineMeshDict")
         copy_path = os.path.join(template_dir, "system", "refineLayerMeshDict")
         os.system(
-            f"foamDictionary -entry castellatedMeshControls/refinementRegions/refinementBox/levels"
+            "foamDictionary -entry castellatedMeshControls/refinementRegions/refinementBox/levels"
             f" -set '( ({refinement} {refinement}) );' {refine_dict_path}"
         )
-        openfoam.mesh.refine_RVE(template_dir, additivefoam_input_dict["layer_box"])
+        openfoam.mesh.refine_RVE(template_dir, layer_box)
 
         # Archive copy of the layer refinement dict
         shutil.copy(refine_dict_path, copy_path)
 
         # Generate refined mesh in region
-        refinement = additivefoam_input_dict["mesh"]["refine_region"]
+        refinement = app.args.refine_region + app.args.refine_layer
         refine_dict_path = os.path.join(template_dir, "system", "refineMeshDict")
         os.system(
-            f"foamDictionary -entry castellatedMeshControls/refinementRegions/refinementBox/levels"
+            "foamDictionary -entry castellatedMeshControls/refinementRegions/refinementBox/levels"
             f" -set '( ({refinement} {refinement}) );' {refine_dict_path}"
         )
-        openfoam.mesh.refine_RVE(template_dir, additivefoam_input_dict["region_box"])
+        openfoam.mesh.refine_RVE(template_dir, region_box)
 
-    ###############################
-    # Set the material properties #
-    ###############################
-    app.update_material_properties(template_dir)
-
-    ##############################
-    # Copy template to case  dir #
-    ##############################
-    shutil.copytree(template_dir, case_dir, dirs_exist_ok=True)
-
-    ##############################
-    # Set the start and end time #
-    ##############################
-    # 1. Read scan path
-    df = pd.read_csv(new_scan_path_file, sep="\s+")
-
-    # 2. Iterate through rows to determine intersection with
-    # the region's bounding box
-    elapsed_time = 0.0
-    start_time = None
-    end_time = None
-    for index, row in df.iloc[1:].iterrows():
-        # 2A. If scan path row is a scan vector (Pmod == 1)
-        if row["Mode"] == 0:
-            v = row["tParam"]
-            x1 = row["X(m)"]
-            y1 = row["Y(m)"]
-            x0 = df.iloc[index - 1]["X(m)"]
-            y0 = df.iloc[index - 1]["Y(m)"]
-            xs = np.linspace(x0, x1, 1000)
-            ys = np.linspace(y0, y1, 1000)
-            in_region = any(
-                (xs > bbDict["bb_min"][0])
-                & (xs < bbDict["bb_max"][0])
-                & (ys > bbDict["bb_min"][1])
-                & (ys < bbDict["bb_max"][1])
-            )
-            if in_region:
-                end_time = None
-            if in_region and (start_time is None):
-                start_time = elapsed_time
-            elif (not in_region) and (end_time is None):
-                end_time = elapsed_time
-            elapsed_time += np.linalg.norm(np.array([x1 - x0, y1 - y0])) / v
-
-        # 2B. If scan path row is a spot (Pmod == 0)
-        if row["Mode"] == 1:
-            elapsed_time += row["tParam"]
-
-    # 3. If all vectors or no vectors are in the region,
-    # then set the start and end time
-    if start_time is None:
-        start_time = 0.0
-    if end_time is None:
-        end_time = elapsed_time
-
-    # 4. Set the simulation parameters:
-    # - start and end times of the simulation
-    # - name of initial time-step directory
-    # - scan path directory
-    start_time = np.round(start_time, 5)
-    end_time = np.round(end_time, 5)
-    os.system(
-        f"foamDictionary -entry startTime -set {start_time} "
-        + f"{case_dir}/system/controlDict"
-    )
-    os.system(
-        f"foamDictionary -entry endTime -set {end_time} "
-        + f"{case_dir}/system/controlDict"
-    )
-    os.system(
-        f"foamDictionary -entry writeInterval -set {np.round(0.5 * (end_time - start_time), 5)} "
-        + f"{case_dir}/system/controlDict"
-    )
-    shutil.move(os.path.join(case_dir, "0"), os.path.join(case_dir, f"{start_time}"))
-    os.system(
-        "foamDictionary -entry beam/pathName -set"
-        + f""" '"{path_name}"' """
-        + f"{case_dir}/constant/heatSourceDict"
-    )
+    # Copy template to case dir and then update the case parameters
+    shutil.copytree(template_dir_abs, case_dir, dirs_exist_ok=True)
+    app.update_beam_spot_size(part, case_dir)
+    app.update_material_properties(case_dir)
+    app.update_region_start_and_end_times(case_dir, bb_dict, path_name)
+    app.update_heatsource_scanfile(case_dir, path_name)
 
     return
 
 
 def main():
+    """Configure all additivefoam/solidification_region_stl case directories into
+    valid AdditiveFOAM cases
+    """
 
     # Create app instance
     app = AdditiveFOAM("solidification_region_stl")
