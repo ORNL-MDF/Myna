03ea38c8ca69d1b525c60d03a45bab0e941f67f0
Gerry Knapp <knappgl@ornl.gov>
2025-03-26 17:25:20 -0400

    add cubit application directory with vtk_to_exodus_region app
    Co-authored-by: Sagar Bhatt <sbhatt@anl.gov>
    Co-authored-by: Mark Messner <messner@anl.gov>

---
+++ src/myna/application/cubit/vtk_to_exodus_region/execute.py
@@ -0,0 +1,21 @@
+#
+# Copyright (c) 2024 Oak Ridge National Laboratory.
+#
+# This file is part of Myna. For details, see the top-level license
+# at https://github.com/ORNL-MDF/Myna/LICENSE.md.
+#
+# License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause.
+#
+
+from myna.application.cubit.vtk_to_exodus_region import CubitVtkToExodusApp
+
+
+def execute():
+    """Configure all cubit/vtk_to_exodus case directories"""
+    # Create app instance and configure all cases
+    app = CubitVtkToExodusApp()
+    app.mesh_all_cases()
+
+
+if __name__ == "__main__":
+    execute()
