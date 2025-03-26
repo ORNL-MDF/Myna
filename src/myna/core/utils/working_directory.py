13ae9e5143fb9886f39da0f7ed0dd2c5f6564858
Gerry Knapp <knappgl@ornl.gov>
2025-03-26 17:23:46 -0400

    move working directory context manager to myna.core.utils

---
+++ src/myna/core/utils/working_directory.py
@@ -0,0 +1,23 @@
+#
+# Copyright (c) 2024 Oak Ridge National Laboratory.
+#
+# This file is part of Myna. For details, see the top-level license
+# at https://github.com/ORNL-MDF/Myna/LICENSE.md.
+#
+# License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause.
+#
+"""Tools for managing working directories"""
+import os
+from pathlib import Path
+import contextlib
+
+
+@contextlib.contextmanager
+def working_directory(path):
+    """Changes working directory and returns to previous on exit."""
+    prev_cwd = Path.cwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
