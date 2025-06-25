544e6c8597561ead23fb2727f7c81d6d927d9bdc
Gerry Knapp <knappgl@ornl.gov>
2025-06-25 13:53:54 -0400

    test solidification_part with all database options

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -20,14 +20,48 @@ def get_example_dir(example_name):
     return os.path.join(myna_path, "examples", example_name)
 
 
+def get_run_cmd_str():
+    """Returns a command string to launch and clean an example case. Assumes a clean
+    directory to begin with"""
+    return (
+        "myna config --output ic.yaml"
+        + " && myna run --input ic.yaml"
+        + " && rm -rf myna_output myna_resources ic.yaml"
+    )
+
+
 @pytest.mark.examples
-def test_solidification():
+def test_solidification_part():
     """This checks the solidification_part example"""
     with working_directory(get_example_dir("solidification_part")):
         output = subprocess.run(
-            "myna config --output ic.yaml"
-            + " && myna run --input ic.yaml"
-            + " && rm -rf myna_output myna_resources ic.yaml",
+            get_run_cmd_str(),
+            shell=True,
+            check=True,
+            capture_output=True,
+        ).stdout.decode("utf-8")
+    assert "All output files are valid" in output
+
+
+@pytest.mark.examples
+def test_solidification_part_hdf5():
+    """This checks the solidification_part example"""
+    with working_directory(get_example_dir("solidification_part_hdf5")):
+        output = subprocess.run(
+            get_run_cmd_str(),
+            shell=True,
+            check=True,
+            capture_output=True,
+        ).stdout.decode("utf-8")
+    assert "All output files are valid" in output
+
+
+@pytest.mark.examples
+def test_solidification_part_json():
+    """This checks the solidification_part example"""
+    with working_directory(get_example_dir("solidification_part_json")):
+        output = subprocess.run(
+            get_run_cmd_str(),
             shell=True,
             check=True,
             capture_output=True,
