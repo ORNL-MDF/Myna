23987ee2cd0af3067e06786a61ab98789b1c255b
Gerry Knapp <knappgl@ornl.gov>
2025-06-26 16:16:38 -0400

    CI for running examples (#101)
    * add CI based on docker container to test apps
    
    * expand pytest for checking executables
    
    * add initial pytest --examples and CI
    
    * fixup: handle detached head in git check
    
    * add CI job dependencies
    
    * add parallel pytest with pytest-xdist
    
    * test solidification_part with all database options
    
    * parallel CI for running examples, additiveFoam tests
    
    * fixup: typo in CI
    
    * fixup
    
    * add microstructure region test, comment/clean CI.yml
    
    * redo markers
    
    * simplify pytest configuration in pyproject.toml
    
    * examples use myna functions instead of subprocess calls
    
    * fixup: use $GITHUB_WORKSPACE and lint test_examples.py
    
    * delete the example test tmp files even on test fail
    
    * add verbose output in CI

---
+++ tests/test_examples.py
@@ -0,0 +1,96 @@
+"""Tests for configuring and running example cases. Each example test should be marked
+with `@pytest.mark.examples`. By default, it is assumed that each example uses a single
+processor. Additional markers should be specified to indicate test complexity:
+
+- @pytest.mark.apps: requires external dependency
+- @pytest.mark.parallel: example uses 2 processors instead of 1
+"""
+
+#
+# Copyright (c) 2024 Oak Ridge National Laboratory.
+#
+# This file is part of Myna. For details, see the top-level license
+# at https://github.com/ORNL-MDF/Myna/LICENSE.md.
+#
+# License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause.
+#
+import os
+import shutil
+import pytest
+from myna.core.utils import working_directory
+from myna.core.workflow.config import config
+from myna.core.workflow.run import run
+
+
+def get_example_dir(example_name):
+    """Creates a temporary directory for running the example"""
+    test_path = os.path.dirname(os.path.abspath(__file__))
+    myna_path = os.path.abspath(os.path.join(test_path, ".."))
+    return os.path.join(myna_path, "examples", example_name)
+
+
+def run_example_test(example_name):
+    """Perform a test run of the example case in a temporary directory, then clean up"""
+
+    # Setup the temporary directory for the example to run.
+    # The `tmp` directories need to be in `examples/` to find the database files.
+    example_dir = get_example_dir(example_name)
+    tmp_dir = example_dir + "_tmp"
+    os.makedirs(tmp_dir)
+    shutil.copyfile(
+        os.path.join(example_dir, "input.yaml"), os.path.join(tmp_dir, "input.yaml")
+    )
+
+    # Run and clean the example
+    try:
+        with working_directory(tmp_dir):
+            config("input.yaml")
+            run("input.yaml")
+    finally:
+        shutil.rmtree(tmp_dir, ignore_errors=True)
+
+
+@pytest.mark.apps
+@pytest.mark.examples
+def test_solidification_part():
+    """This checks the solidification_part example. Estimated runtime ~20s."""
+    run_example_test("solidification_part")
+
+
+@pytest.mark.apps
+@pytest.mark.examples
+def test_solidification_part_hdf5():
+    """This checks the solidification_part example. Estimated runtime ~20s."""
+    run_example_test("solidification_part_hdf5")
+
+
+@pytest.mark.apps
+@pytest.mark.examples
+def test_solidification_part_json():
+    """This checks the solidification_part example. Estimated runtime ~20s."""
+    run_example_test("solidification_part_json")
+
+
+@pytest.mark.apps
+@pytest.mark.examples
+@pytest.mark.parallel
+def test_microstructure_region():
+    """This checks the microstructure_region example, which checks:
+
+        - additivefoam/solidification_region_reduced
+        - exaca/microstructure_region
+
+    Estimated runtime ~40s."""
+    run_example_test("microstructure_region")
+
+
+@pytest.mark.apps
+@pytest.mark.examples
+@pytest.mark.parallel
+def test_solidification_region_reduced_stl():
+    """This checks the solidification_region_reduced_stl example, which checks:
+
+        - additivefoam/solidification_region_reduced_stl
+
+    Estimated runtime ~40s."""
+    run_example_test("solidification_region_reduced_stl")
---
+++ tests/test_executables.py
@@ -0,0 +1,52 @@
+#
+# Copyright (c) 2024 Oak Ridge National Laboratory.
+#
+# This file is part of Myna. For details, see the top-level license
+# at https://github.com/ORNL-MDF/Myna/LICENSE.md.
+#
+# License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause.
+#
+import pytest
+import subprocess
+
+
+@pytest.mark.apps
+def test_exaca():
+    """This test checks if ExaCA is in the current environment, which is the
+    preferred method for accessing the executable in the ExaCA applications"""
+    # This assumes it is on the path
+    output = subprocess.run(
+        'ExaCA | grep "ExaCA version"', shell=True, check=False, capture_output=True
+    ).stdout.decode("utf-8")
+    assert "ExaCA version" in output
+
+
+@pytest.mark.apps
+def test_openfoam_10():
+    """This test checks if OpenFOAM-10 is activated in the current environment,
+    which is required for the AdditiveFOAM apps"""
+    output = subprocess.run(
+        'checkMesh -help | grep "Using:"', shell=True, check=False, capture_output=True
+    ).stdout.decode("utf-8")
+    assert "OpenFOAM-10" in output
+
+
+@pytest.mark.apps
+def test_additivefoam():
+    """This test checks if `additiveFoam` is on the current path"""
+    output = subprocess.run(
+        'additiveFoam | grep "AdditiveFOAM Information"',
+        shell=True,
+        check=False,
+        capture_output=True,
+    ).stdout.decode("utf-8")
+    assert "AdditiveFOAM" in output
+
+
+@pytest.mark.apps
+def test_thesis():
+    """This test checks if `3DThesis` is on the current path"""
+    output = subprocess.run(
+        '3DThesis | grep "3DThesis"', shell=True, check=False, capture_output=True
+    ).stderr.decode("utf-8")
+    assert "3DThesis" in output
